b69198581dbb412e869993e7f80bc5a2464430f3
Rothana Choun <rothana.choun7@gmail.com>
2019-02-28 22:41:51 +0700

    fix(): import live module

--- components/index.js
+++ components/index.js
@@ -1,14 +1,15 @@
-import Video from './Video'
-export * from './PlayButton'
-export * from './Controls'
-export * from './ControlBar'
-export * from './Scrubber'
-export * from './Time'
-export * from './Loading'
-export * from './TopBar'
-export * from './ToggleIcon'
-export * from './ProgressBar'
-export * from './ScrollView'
-export * from './Container'
+import Video from "./Video";
+export * from "./PlayButton";
+export * from "./Controls";
+export * from "./ControlBar";
+export * from "./Scrubber";
+export * from "./Time";
+export * from "./Loading";
+export * from "./TopBar";
+export * from "./ToggleIcon";
+export * from "./ProgressBar";
+export * from "./ScrollView";
+export * from "./Container";
+export * from "./Live";
 
-export default Video
+export default Video;
